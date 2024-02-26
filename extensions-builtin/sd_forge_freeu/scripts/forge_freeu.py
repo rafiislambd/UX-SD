1a09708447473bc327c1bce2b6a4f7e145f65299
rafiislambd <143995590+rafiislambd@users.noreply.github.com>
2024-02-26 23:54:36 +0600

    Update forge_freeu.py

--- extensions-builtin/sd_forge_freeu/scripts/forge_freeu.py
+++ extensions-builtin/sd_forge_freeu/scripts/forge_freeu.py
@@ -55,10 +55,10 @@ class FreeUForForge(scripts.Script):
     def ui(self, *args, **kwargs):
         with gr.Accordion(open=False, label=self.title()):
             freeu_enabled = gr.Checkbox(label='Enabled', value=False)
-            freeu_b1 = gr.Slider(label='B1', minimum=0, maximum=2, step=0.01, value=1.01)
-            freeu_b2 = gr.Slider(label='B2', minimum=0, maximum=2, step=0.01, value=1.02)
-            freeu_s1 = gr.Slider(label='S1', minimum=0, maximum=4, step=0.01, value=0.99)
-            freeu_s2 = gr.Slider(label='S2', minimum=0, maximum=4, step=0.01, value=0.95)
+            freeu_b1 = gr.Slider(label='B1', minimum=0, maximum=2, step=0.01, value=1.3)
+            freeu_b2 = gr.Slider(label='B2', minimum=0, maximum=2, step=0.01, value=1.4)
+            freeu_s1 = gr.Slider(label='S1', minimum=0, maximum=4, step=0.01, value=0.9)
+            freeu_s2 = gr.Slider(label='S2', minimum=0, maximum=4, step=0.01, value=0.2)
 
         return freeu_enabled, freeu_b1, freeu_b2, freeu_s1, freeu_s2
 
